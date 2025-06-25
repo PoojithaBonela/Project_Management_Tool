292fb45a1c90571734c2de90081c4f42ee996edf
Poojitha Bonela <poojithabonela19@gmail.com>
2025-06-26 00:22:25 +0530

    Update db.js

--- Project-Management-Tool/config/db.js
+++ Project-Management-Tool/config/db.js
@@ -1,9 +1,16 @@
+require('dotenv').config({ path: '.env.demo' });  // explicitly load the file
+
 const { Sequelize } = require('sequelize');
 
-// Replace with your actual MySQL username and password
-const sequelize = new Sequelize('task_manager', 'root', 'poojitha1905', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+  }
+);
 
 module.exports = sequelize;
